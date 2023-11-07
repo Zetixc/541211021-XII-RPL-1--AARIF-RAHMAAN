61cfa434c75a81909f4a72a048a1bce71be15938
Zetixc <faqiihaarif@gmail.com>
2023-11-07 14:44:04 +0700

    lkpd2

--- router/users.js
+++ router/users.js
@@ -1,38 +1,14 @@
 const express = require("express");
-
 const router = express.Router();
 
-let users = [
-  { id: 1, nama: "A", email: "a@gmail.com" },
-  { id: 2, nama: "B", email: "b@gmail.com" },
-];
+const usercontroller = require("../controllers/user");
 
-router.get("/users", (req, res) => {
-  res.json(users);
-});
+router.get("/users", usercontroller.index);
 
-router.post("/user", (req, res) => {
-  users.push(req.body);
-  res.json(users);
-});
+router.post("/user", usercontroller.store);
 
-router.put("/user/:id", (req, res) => {
-  const id = req.params.id;
-  users.filter((user) => {
-    if (user.id == id) {
-      user.nama = req.body.nama;
-      user.email = req.body.email;
-      return user;
-    }
-  });
-  res.json(users);
-});
+router.put("/user/:id", usercontroller.update);
 
-router.delete("/user/:id", (req, res) => {
-  const id = req.params.id;
-  users = users.filter((user) => user.id != id);
-
-  res.json(users);
-});
+router.delete("/user/:id", usercontroller.delete);
 
 module.exports = router;
